f03f4ea7b115b9cf1299c95a2d86f72197c7fa5f
ritikadas10 <das.ritika13@gmail.com>
2022-03-03 05:57:08 +0000

    Modified version in config file

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    version = '1.0.1';
+    version = '1.0.2';
     team = 'Your Team Name';
 }
 module.exports = AppConfig;
